ef82f0da1a48701a7abe5f06c93f4c91d5a0fd12
Ari <arijusgrotuzas@gmail.com>
2022-11-10 16:33:10 +0100

    Added basic RISC-V instructions (completed Task 1)

--- src/Methods/IsaSim.java
+++ src/Methods/IsaSim.java
@@ -1,5 +1,6 @@
 package Methods;
 
+import java.awt.desktop.ScreenSleepEvent;
 import java.io.*;
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
@@ -9,50 +10,76 @@ public class IsaSim {
     static int pc;
     static int[] reg = new int[32];
     static final ArrayList<Integer> program = new ArrayList<>();
+    private static boolean execute = true;
 
-    // Here the first program hard coded as an array
-    static int progr[] = {
-            // As minimal RISC-V assembler example
-            0x00200093, // addi x1 x0 2
-            0x00300113, // addi x2 x0 3
-            0x002081b3, // add x3 x1 x2
-    };
-
+    /**
+     *
+     * @param opcode F
+     * @param rd F
+     * @param rs1 F
+     * @param rs2 F
+     * @param iImm F
+     * @param uImm F
+     * @param funct3 F
+     */
     public static void executeOP(int opcode, int rd, int rs1, int rs2, int iImm, int uImm, int funct3){
         switch (opcode) {
             case 0x37 -> reg[rd] = (uImm << 12); // LUI
+            case 0x17 -> reg[rd] = pc + (uImm << 12); // AUIPC
             case 0x13 -> { // ADDI - SRAI
                 switch (funct3) {
                     case 0x0 -> reg[rd] = reg[rs1] + iImm; // ADDI
-                    case 0x2 -> { // SLTI
-                        if (reg[rs1] < iImm){
-                            reg[rd] = 0x1;
-                        }
-                        else {
-                            reg[rd] = 0x0;
-                        }
-                    }
-                    case 0x3 -> reg[rd] = reg[rs1]; // SLTIU *
+                    case 0x2 -> reg[rd] = (reg[rs1] < iImm) ? 1:0; // SLTI
+                    case 0x3 -> reg[rd] = (Integer.compareUnsigned(reg[rs1], iImm) < 0) ? 1:0; // SLTIU
                     case 0x4 -> reg[rd] = reg[rs1] ^ iImm; // XORI
                     case 0x6 -> reg[rd] = reg[rs1] | iImm; // ORI
                     case 0x7 -> reg[rd] = reg[rs1] & iImm; // ANDI
-                    case 0x1 -> reg[rd] = reg[rs1] + iImm; // SLLI
+                    case 0x1 -> reg[rd] = reg[rs1] << (iImm & 0x01f); // SLLI
                     case 0x5 -> { // SRLI - SRAI
-                        int upperImm = iImm >> 5;
-                        int lowerImm = iImm & 0x01f;
-                        switch (upperImm) {
-                            case 0x20 -> reg[rd] = reg[rs1] >> lowerImm; // SRLI
-                            case 0x0 -> reg[rd] = reg[rs1] >> lowerImm; // SRAI *
+                        switch ((iImm >> 5)) {
+                            case 0x0 -> reg[rd] = reg[rs1] >>> (iImm & 0x01f); // SRLI
+                            case 0x20 -> reg[rd] = reg[rs1] >> (iImm & 0x01f); // SRAI
                         }
                     }
                 }
             }
-            case 0x33 -> reg[rd] = reg[rs1] + reg[rs2]; // ADD
-            case 0x73 -> System.out.println("Ecall"); // ECALL
+            case 0x33 -> { // ADD - AND
+                switch (funct3){
+                    case 0x0 -> { // ADD - SUB
+                        switch ((iImm >> 5)){
+                            case 0x20 -> reg[rd] = reg[rs1] - reg[rs2]; // SUB
+                            case 0x0 -> reg[rd] = reg[rs1] + reg[rs2]; // ADD
+                        }
+                    }
+                    case 0x1 -> reg[rd] = reg[rs1] << reg[rs2]; // SLL
+                    case 0x2 -> reg[rd] = (reg[rs1] < reg[rs2]) ? 1:0;// SLT
+                    case 0x3 -> reg[rd] = (Integer.compareUnsigned(reg[rs1], reg[rs2]) < 0) ? 1:0; // SLTU
+                    case 0x4 -> reg[rd] = reg[rs1] ^ reg[rs2]; // XOR
+                    case 0x5 -> {
+                        switch ((iImm >> 5)) {
+                            case 0x0 -> reg[rd] = reg[rs1] >>> reg[rs2]; // SRL
+                            case 0x20 -> reg[rd] = reg[rs1] >> reg[rs2]; // SRA
+                        }
+                    }
+                    case 0x6 -> reg[rd] = reg[rs1] | reg[rs2]; // OR
+                    case 0x7 -> reg[rd] = reg[rs1] & reg[rs2]; // AND
+                }
+            }
+            case 0x73 -> { // ECALL
+                switch (reg[17]){
+                    case 0xa -> execute = false; // EXIT
+                    default -> System.out.println("ECALL");
+                }
+            }
             default -> System.out.println("Unrecognized operation: " + opcode);
         }
     }
 
+    /**
+     *
+     * @param filename String containing the name of the binary file
+     * @throws IOException f
+     */
     public static void readProgram(String filename) throws IOException {
         try (DataInputStream dis = new DataInputStream(new FileInputStream(filename))) {
             byte[] instr = new byte[4];
@@ -78,9 +105,12 @@ public class IsaSim {
             readProgram(args[0]);
         }
         else{
-            readProgram("tests/addlarge.bin");
+            readProgram("tests/shift2.bin");
         }
 
+        int p = -100;
+        System.out.println(Integer.toBinaryString(p));
+        System.out.println(Integer.toBinaryString(p >>> 5));
         pc = 0;
 
         // For loop for executing the program
@@ -95,7 +125,7 @@ public class IsaSim {
             int iImm = (instr >> 20);
             int uImm = (instr >> 12);
 
-            System.out.println("Operation invoked: " + opcode);
+            //System.out.println("Operation invoked: " + opcode);
             executeOP(opcode, rd, rs1, rs2, iImm, uImm, funct3);
 
             // Increment the PC
@@ -106,7 +136,7 @@ public class IsaSim {
                 System.out.print(j + " ");
             }
             System.out.println();
-        } while ((pc >> 2) < program.size());
+        } while ((pc >> 2) < program.size() && execute);
 
         System.out.println("Program exit");
     }
